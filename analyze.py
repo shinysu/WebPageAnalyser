23944c5ac6bb6a91a710a69b776ba1309cf408ef
shinysu <shiny.suresh@gmail.com>
2020-01-07 15:33:13 +0530

    Initial commit

---
+++ analyze.py
@@ -0,0 +1,51 @@
+from bs4.element import Comment
+from string import punctuation
+
+
+def clean_string(inp_str):
+    st = str.maketrans('', '', punctuation)
+    return inp_str.translate(st).lower().strip().split()
+
+
+def remove_stop_words(inp_dict):
+    stop_word_list = get_stop_words()
+    for word in stop_word_list:
+        if word in inp_dict.keys():
+            del inp_dict[word]
+    return inp_dict
+
+
+def get_stop_words():
+    stop_word_list = []
+    with open('stopwords.txt','r') as file:
+        for word in file:
+            stop_word_list.append(word.strip('\n'))
+    return stop_word_list
+
+
+def get_word_count(inp_str):
+    st = clean_string(inp_str)
+    d = {}
+    total_count = len(st)
+    for word in st:
+        d[word] = d.get(word, 0) + 1
+    unique_count = len(d.items())
+    cleaned_d = remove_stop_words(d)
+    sorted_d = dict(sorted(cleaned_d.items(), key=lambda kv:kv[1], reverse=True))
+    return total_count, unique_count, list(sorted_d.keys())[:5]
+
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'meta', '[document]','noscript','header', 'html', 'input']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(soup):
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    return u" ".join(t.strip() for t in visible_texts)
+
+
---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template,request,url_for,redirect
+import requests
+from bs4 import BeautifulSoup
+from analyze import tag_visible, get_word_count, text_from_html
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('analyzer.html')
+
+
+@app.route('/get_info',methods=['POST'])
+def get_info():
+    url = request.form['input_url']
+    try:
+        source = requests.get(url, timeout=5)
+        soup = BeautifulSoup(source.text, 'html.parser')
+        page_text = text_from_html(soup)
+        total_count, unique_count, top_words = get_word_count(page_text)
+    except requests.ConnectionError as e:
+        error_message = "Connection Error. Make sure you are connected to Internet"
+        return redirect(url_for('error',error_message=error_message+":"+str(e)))
+    except requests.Timeout as e:
+        error_message = "Timeout Error"
+        return redirect(url_for('error', error_message=error_message+":"+str(e)))
+    except requests.RequestException as e:
+        error_message = "General Error"
+        return redirect(url_for('error', error_message=error_message+":"+str(e)))
+    except KeyboardInterrupt as e:
+        error_message = "Keyboard interrupt"
+        return redirect(url_for('error', error_message=error_message+":"+str(e)))
+
+    return render_template('analyzer.html',total_count=total_count,unique_count=unique_count,top_words=top_words)
+
+
+@app.route('/error/<error_message>')
+def error(error_message):
+    return render_template('error.html',error_message=error_message)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
